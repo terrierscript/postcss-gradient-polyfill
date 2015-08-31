3daedb285d85bc088dd832ce3a82f2762392d47c
inuscript <inuscript@gmail.com>
2015-08-31 18:51:11 +0900

    prototype

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
 var postcss = require('postcss');
 var gradient = require("gradient-parser")
-var color = require("color")
+var Color = require("color")
 module.exports = postcss.plugin('postcss-gradient-failover', function () {
   return function (css) {
     css.eachDecl('background', function (decl) {
       var colors = parseGradientColors(decl.value)
-      generateFailoverColor(colors)
-      // console.log(decl.value)
-      // var sizes = postcss.list.space(decl.value);
-      // if ( sizes.length === 1 ) sizes[1] = sizes[0];
-      // 
-      // decl.cloneBefore({ prop: 'width',  value: sizes[0] });
-      // decl.cloneBefore({ prop: 'height', value: sizes[1] });
-      // 
-      // decl.removeSelf();
+      var mixedColor = generatePolyfillColor(colors)
+      decl.cloneBefore({
+        prop: 'background',
+        value: mixedColor
+      })
     });
   };
 });
 function parseGradientColors(value){
   var colors = gradient.parse(value)[0].colorStops
   return colors.map(function(ast){
-    if(ast.type === "hex"){
-      return "#" + value;
+    try{
+      if(ast.type === "hex"){
+        return Color("#" + ast.value);
+      }
+    }catch(e){
+      console.log(e)
+      return null
     }
-    return value
+  }).filter(function(c){ // comapct
+    return c
   })
 }
 
-function generateFailoverColor(colors){
-  colors.reduce(function(color1, color2){
-    if(){
-      
-    }
-  }, null)
+function generatePolyfillColor(colors){
+  return colors[0].mix(colors[1]).hexString()
 }
--- test/index.js
+++ test/index.js
@@ -12,7 +12,7 @@ describe('postcss-size', function () {
   it('use median color', function () {
     test(
       'a{ background: linear-gradient(#000, #fff);}', 
-      'a{ background:#666666; background: linear-gradient(#000, #fff);}'
+      'a{ background: #808080; background: linear-gradient(#000, #fff);}'
     );
   })
 })
